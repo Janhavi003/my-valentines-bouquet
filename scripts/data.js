d429510467c365aef189f030f7d9f393271b6af8
janhavi003 <janhavi.hivarekar@kalvium.community>
2026-02-25 10:51:49 +0530

    Added bouquet assets and data models

---
+++ scripts/data.js
@@ -0,0 +1,33 @@
+export const flowers = [
+  {
+    name: "Blush Rose",
+    image: "assets/flowers/rose.png"
+  },
+  {
+    name: "Pink Tulip",
+    image: "assets/flowers/tulip.png"
+  },
+  {
+    name: "White Daisy",
+    image: "assets/flowers/daisy.png"
+  },
+  {
+    name: "Soft Peony",
+    image: "assets/flowers/peony.png"
+  }
+];
+
+export const vases = [
+  {
+    name: "Glass Vase",
+    image: "assets/vases/glass.png"
+  },
+  {
+    name: "Ceramic Vase",
+    image: "assets/vases/ceramic.png"
+  },
+  {
+    name: "Pastel Vase",
+    image: "assets/vases/pastel.png"
+  }
+];
